02c2517c21c470eb171a88aea66ad605562a61ae
astlan2500 <143513354+astlan2500@users.noreply.github.com>
2024-04-24 14:39:36 +0200

    hei

---
+++ src/main/java/com/example/oblig3_ferdig/Oblig3FerdigApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig3_ferdig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3FerdigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig3FerdigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oblig3_ferdig/Oblig3FerdigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig3_ferdig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3FerdigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
